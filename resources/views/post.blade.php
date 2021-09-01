590f7a4c8c9c292101ab4ffe38f1070b94b35cfd
J💎 Green <jgreen@JHG.local>
2021-09-01 03:58:21 -0500

    initial commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,13 @@
+<title>This is my blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+
+<article>
+    <h1><a href="/post">My First Post</a></h1>
+    <p>Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. </p>
+</article>
+
+<a href="/">Go Back</a>
+
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+
+<title>This is my blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+        <p>Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third  Post</a></h1>
+        <p>Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. Just Some Text. </p>
+    </article>
+
+</body>

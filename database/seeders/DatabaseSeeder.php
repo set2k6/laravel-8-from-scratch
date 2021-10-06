b0f4a26ee6f77cb77b2e652cca1bd0a6cf1d2886
John Green <john@johngreen.dev>
2021-10-06 02:41:58 -0500

    created category and post factories and seeders for both

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,52 +18,60 @@ class DatabaseSeeder extends Seeder
     public function run()
 
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
+//        User::truncate();
+//        Post::truncate();
+//        Category::truncate();
 
-        $user = User::factory()->create();
-
-        $personal = category::create([
-            'name'=> 'Personal',
-            'slug'=> 'personal'
+        $user = User::factory()->create([
+            'name' => 'John Green'
         ]);
 
-        $family = category::create([
-            'name'=> 'Family',
-            'slug'=> 'family'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
 
-        $work = category::create([
-            'name'=> 'Work',
-            'slug'=> 'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-first-post',
-            'excerpt' => 'Lorem Ipsum daosigan sfnasf',
-            'body' => '<p>jasdf sdfpasuf spd fapsdfhsviuan ufwnf auena fsu dgha ghasdug agh ap gpa ghapsdguhasdg ahsiguhasighda</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My Personal Post',
-            'slug' => 'my-second-post',
-            'excerpt' => 'Lorem Ipsum daosigan sfnasf',
-            'body' => '<p>jasdf sdfpasuf spd fapsdfhsviuan ufwnf auena fsu dgha ghasdug agh ap gpa ghapsdguhasdg ahsiguhasighda</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-third-post',
-            'excerpt' => 'Lorem Ipsum daosigan sfnasf',
-            'body' => '<p>jasdf sdfpasuf spd fapsdfhsviuan ufwnf auena fsu dgha ghasdug agh ap gpa ghapsdguhasdg ahsiguhasighda</p>'
-        ]);
+//        $user = User::factory()->create();
+//
+//        $personal = category::create([
+//            'name'=> 'Personal',
+//            'slug'=> 'personal'
+//        ]);
+//
+//        $family = category::create([
+//            'name'=> 'Family',
+//            'slug'=> 'family'
+//        ]);
+//
+//        $work = category::create([
+//            'name'=> 'Work',
+//            'slug'=> 'work'
+//        ]);
+//
+//        Post::create([
+//            'user_id' => $user->id,
+//            'category_id' => $family->id,
+//            'title' => 'My Family Post',
+//            'slug' => 'my-first-post',
+//            'excerpt' => 'Lorem Ipsum daosigan sfnasf',
+//            'body' => '<p>jasdf sdfpasuf spd fapsdfhsviuan ufwnf auena fsu dgha ghasdug agh ap gpa ghapsdguhasdg ahsiguhasighda</p>'
+//        ]);
+//
+//        Post::create([
+//            'user_id' => $user->id,
+//            'category_id' => $personal->id,
+//            'title' => 'My Personal Post',
+//            'slug' => 'my-second-post',
+//            'excerpt' => 'Lorem Ipsum daosigan sfnasf',
+//            'body' => '<p>jasdf sdfpasuf spd fapsdfhsviuan ufwnf auena fsu dgha ghasdug agh ap gpa ghapsdguhasdg ahsiguhasighda</p>'
+//        ]);
+//
+//        Post::create([
+//            'user_id' => $user->id,
+//            'category_id' => $work->id,
+//            'title' => 'My Work Post',
+//            'slug' => 'my-third-post',
+//            'excerpt' => 'Lorem Ipsum daosigan sfnasf',
+//            'body' => '<p>jasdf sdfpasuf spd fapsdfhsviuan ufwnf auena fsu dgha ghasdug agh ap gpa ghapsdguhasdg ahsiguhasighda</p>'
+//        ]);
     }
 }

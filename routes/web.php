c989867959b516c95d4d5cfa2def4a741dd935ad
John Green <john@johngreen.dev>
2021-10-14 03:04:13 -0500

    added alpine js for front end work and set the routes for views and blades

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,8 @@ Route::get('/', function () {
         logger($query->sql, $query->bindings);
     });
     return view('posts',[
-        'posts'=> Post::latest()->with('category','author')->get()
+        'posts'=> Post::latest()->with('category','author')->get(),
+        'categories'=> Category::all()
     ]);
 });
 
@@ -37,13 +38,16 @@ Route::get('posts/{post:slug}', function (Post $post) {
 
 Route::get('categories/{category:slug}', function (Category $category){
         return view('posts',[
-            'posts'=> $category->posts->load(['category', 'author'])
+            'posts'=> $category->posts->load(['category', 'author']),
+            'currentCategory'=> $category,
+            'categories'=> Category::all()
         ]);
  });
 
 Route::get('authors/{author:username}', function (User $author){
     return view('posts',[
-        'posts'=> $author->posts->load(['category', 'author'])
+        'posts'=> $author->posts->load(['category', 'author']),
+        'categories'=> Category::all()
     ]);
 });
 

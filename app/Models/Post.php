8f526079a689e51a89c2f00a56af1eee95cfedde
John Green <john@johngreen.dev>
2021-10-20 04:48:07 -0500

    cleaned up the search method and created new post controller to handle queries

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,6 +12,16 @@ class Post extends Model
     use HasFactory;
     protected $guarded = [];
 
+    protected $with = ['category','author'];
+
+    public function scopeFilter($query, array $filters){
+        if ($filters['search'] ?? false) {
+            $query
+                ->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('body', 'like', '%' . request('search') . '%');
+        }
+    }
+
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);

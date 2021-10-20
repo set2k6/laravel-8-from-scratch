5c0eb12e7eb30bf7a6ecbe563fd5e81f218588f4
John Green <john@johngreen.dev>
2021-10-20 03:23:07 -0500

    got search functionality implemented

--- resources/views/_post-header.blade.php
+++ resources/views/_post-header.blade.php
@@ -35,23 +35,16 @@
         </div>
 
         <!-- Other Filters -->
-{{--        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">--}}
-{{--            <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">--}}
-{{--                <option value="category" disabled selected>Other Filters--}}
-{{--                </option>--}}
-{{--                <option value="foo">Foo--}}
-{{--                </option>--}}
-{{--                <option value="bar">Bar--}}
-{{--                </option>--}}
-{{--            </select>--}}
-{{--            <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;"/>--}}
-{{--        </div>--}}
+{{-- --}}
 
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
             <form method="GET" action="#">
-                <input type="text" name="search" placeholder="Find something"
-                       class="bg-transparent placeholder-black font-semibold text-sm">
+                <input type="text"
+                       name="search"
+                       placeholder="Find something"
+                       class="bg-transparent placeholder-black font-semibold text-sm"
+                       value="{{ request('search') }}">
             </form>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $posts = Post::latest();
+
+    if (request('search')) {
+        $posts
+        ->where('title', 'like', '%'. request('search') . '%')
+        ->orWhere('body', 'like', '%'. request('search') . '%');
+    }
 
     DB::listen(function ($query){
         logger($query->sql, $query->bindings);
     });
     return view('posts',[
-        'posts'=> Post::latest()->with('category','author')->get(),
+        'posts'=> $posts ->get(),
         'categories'=> Category::all()
     ]);
 })->name('home');

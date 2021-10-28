2fe8b7018a72b215f882b5d4b570490bcb7c2415
John Green <john@johngreen.dev>
2021-10-28 05:15:20 -0500

    implemented new eloquent queries and updated models. Cleaned up some routes

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,14 +15,28 @@ class Post extends Model
     protected $with = ['category','author'];
 
     public function scopeFilter($query, array $filters){
-        if ($filters['search'] ?? false) {
+        $query->when($filters['search'] ?? false, fn($query, $search) =>
             $query
-                ->where('title', 'like', '%' . request('search') . '%')
-                ->orWhere('body', 'like', '%' . request('search') . '%');
-        }
+                ->where('title', 'like', "%" . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%'));
+
+        $query->when($filters['category'] ?? false, fn($query, $category) =>
+            $query->whereHas('category', fn ($query) =>
+                $query->where('slug', $category))
+        );
+//        if ($filters['search'] ?? false)
+//            $query
+//                ->where('title', 'like', '%' . request('search') . '%')
+//                ->orWhere('body', 'like', '%' . request('search') . '%');
+//
+//            if ($filters['category'] ?? false)
+//                $query
+//                    ->where('title', 'like', '%' . request('search') . '%')
+//                    ->orWhere('body', 'like', '%' . request('search') . '%');
+
     }
 
-    public function category(): BelongsTo
+    public function category()
     {
         return $this->belongsTo(Category::class);
     }

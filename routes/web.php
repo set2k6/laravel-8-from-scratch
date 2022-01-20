5ad167c6764abca7851634158a377b7912c0c9f8
John Green <john@johngreen.dev>
2022-01-19 23:30:53 -0600

    connected an email service api and set the route. Tested and works.

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('newsletter', function(){
+    request()->validate(['email' => 'required|email']);
+
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us20'
+    ]);
+
+    try {
+        $response = $mailchimp->lists->addListMember("a8f425ceba", [
+            "email_address" => request('email'),
+            "status" => "subscribed",
+        ]);
+    } catch (\Exception $e) {
+        throw \Illuminate\Validation\ValidationException::withMessages([
+            'email' => 'This email could not be added to our newsletter list.'
+        ]);
+    }
+
+
+//    $response = $mailchimp ->lists->getAllLists();
+    return redirect('/')->with('success', 'You are now subscribed to our newsletter!');
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
@@ -29,6 +55,8 @@ Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
 Route::post('sessions', [SessionsController::class, 'store'])->middleware('guest');
 
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
+
+
 
 
 //Route::get('authors/{author:username}', function (User $author){
